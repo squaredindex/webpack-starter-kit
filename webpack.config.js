a33338f180f9ac3d26eb888778b12960950b755b
Lev <levdevuk@gmail.com>
2017-12-01 21:12:37 +0000

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path:__dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                  })
+            }
+        ],
+    },
+    devServer: {
+        contentBase:__dirname + "/dist",
+        compress: true,
+        port: 9000,
+        stats: "errors-only"
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '<%= htmlWebpackPlugin.options.title %>',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/project.html'
+        }),
+        new ExtractTextPlugin("app.css")
+    ]
+};
