200ee69e39047e8b47e7f939dab0c04e387b6165
Lev <levdevuk@gmail.com>
2017-12-01 21:28:50 +0000

    Commented out port config of devServer - will now use 8080 by default

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     devServer: {
         contentBase:__dirname + "/dist",
         compress: true,
-        port: 9000,
+        // port: 9000,
         stats: "errors-only"
       },
     plugins: [
